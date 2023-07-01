17a5e896785e7d5d6752d28ce104b2a34a24c236
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-01 10:44:58 +0900

    done

--- exapp/__main__.py
+++ exapp/__main__.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, Response, make_response, send_from_directory
+import os
+from pathlib import Path
+import re
 
 app = Flask(__name__)
+
+
+PUBLIC_DIR = Path(os.getcwd()) / "public"
 
 
 @app.get("/hello")
 def hello() -> str:
     return "hello"
+
+
+@app.route('/<path:path>')
+def download_file(path: str) -> Response:
+    items = path.split("/")  # todo: 雑実装
+    d = "/".join(items[:-1])
+    f = items[-1]
+    return send_from_directory(PUBLIC_DIR / d, f)
+
+
+@app.route("/")
+def index():
+    return send_from_directory(PUBLIC_DIR, "index.html")
 
 
 def main():
